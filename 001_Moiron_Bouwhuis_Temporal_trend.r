c023a1dbf776c71272af4575a562a0273734eb3f
Maria Moiron <mariamoironc@gmail.com>
2024-03-05 00:18:51 +0100

    Update 001_Moiron_Bouwhuis_Temporal_trend.r

--- 001_Moiron_Bouwhuis_Temporal_trend.r
+++ 001_Moiron_Bouwhuis_Temporal_trend.r
@@ -5,7 +5,7 @@
 # The code provided here is sufficient to replicate the results presented in the above paper
 
 ######################################################
-# DATA ANALYSIS OF TEMPORAL TRENDS
+# DATA ANALYSIS OF TEMPORAL TREND IN NUMBER OF NB
 ######################################################
 
 # Loading packages
@@ -17,7 +17,7 @@ library(ggplot2)
 data <- read.table("social_ageing_data.txt", header=TRUE)
 
 #Response variable
-data$N.NB<-as.numeric(data$Data$n.neigbors.4w.ahead.2m) #social data, number of NB in a 2m radious
+data$N.NB<-as.numeric(data$n.neigbors.4w.ahead.2m) #social data, number of NB in a 2m radious
 
 #Covariate
 data$year = as.numeric(data$year)  
@@ -30,13 +30,12 @@ trait.se<-tapply(Data$N.NB,Data$year,standard_error)
 # Plotting linear temporal trend (Figure 2)
 ye <- rgb(0.128,0.128,0.128,0.5) #set colour
 
-{par(mar=c(5,6,2,2))
+par(mar=c(5,6,2,2))
   plot(Yrs, trait.means,xlab= "Year",ylab= expression("Average number of neighbours"),
        pch=21,bg=1,cex.lab=3,col=ye,cex.axis=2,
        ylim=c(1,12),
        cex=2)
-  segments(Yrs,trait.means-trait.se,Yrs,trait.means+trait.se, col = ye)  
-}
+  segments(Yrs,trait.means-trait.se,Yrs,trait.means+trait.se, col = ye)
 
 mbd <- lm(size ~ 1 + year,data = Data, na.action = "na.omit")
 m0<-lm(trait.means~Yrs, na.action = "na.omit")
